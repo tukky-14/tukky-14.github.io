f620dd92798368e2ac30248b9c750739068551c6
tukky-14 <bigoonage0104@gmail.com>
2023-02-19 23:10:28 +0900

    ビルドエラーの修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import CommonHead from '@/components/CommonHead';
 import Header from '@/components/Header';
-import Aboutme from '@/components/AboutMe';
+import AboutMe from '@/components/AboutMe';
 import Works from '@/components/Works';
 import MySkills from '@/components/MySkills';
 import Links from '@/components/Links';
@@ -13,7 +13,7 @@ export default function Home() {
             <main className="bg-gray-100">
                 <Header />
                 <div className="w-4/5 sm:w-2/3 m-auto mb-6">
-                    <Aboutme />
+                    <AboutMe />
                     <Works />
                     <MySkills />
                     <Links />
