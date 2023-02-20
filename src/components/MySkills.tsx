58c0d362f27bca38831315dba512409695cc7c48
tukky-14 <bigoonage0104@gmail.com>
2023-02-20 22:09:11 +0900

    MySkillsにリンクを追加

--- src/components/MySkills.tsx
+++ src/components/MySkills.tsx
@@ -14,17 +14,29 @@ const MySkills = () => {
         <div className="mb-40 pt-10" id="my_skills">
             <h2 className="text-center text-2xl mb-8 m-auto font-bold border-b-4 w-1/2 border-blue-500">MY SKILLS</h2>
             <div className="grid grid-cols-1 sm:grid-cols-3 gap-10">
-                {skillsArray.map((array: { title: string; skills: { name: string; icon: string }[] }, index: number) => (
-                    <div className="border h-72 p-2 bg-white" key={index}>
-                        <p className="text-center font-bold mb-2">{array.title}</p>
-                        {array.skills.map((skill: { name: string; icon: string }, index: number) => (
-                            <div className="flex mb-1" key={index}>
-                                <img className="w-4 mr-1" src={`/skill-icons/${skill.icon}`} alt={`${array.title}のアイコン画像`} />
-                                <p>{skill.name}</p>
-                            </div>
-                        ))}
-                    </div>
-                ))}
+                {skillsArray.map(
+                    (array: { title: string; skills: { name: string; icon: string; link: string }[] }, index: number) => (
+                        <div className="border h-72 p-2 bg-white" key={index}>
+                            <p className="text-center font-bold mb-2">{array.title}</p>
+                            {array.skills.map((skill: { name: string; icon: string; link: string }, index: number) => (
+                                <a
+                                    href={skill.link}
+                                    className="flex mb-1 hover:bg-blue-100 duration-200"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    key={index}
+                                >
+                                    <img
+                                        className="w-4 mr-1"
+                                        src={`/skill-icons/${skill.icon}`}
+                                        alt={`${array.title}のアイコン画像`}
+                                    />
+                                    <p>{skill.name}</p>
+                                </a>
+                            ))}
+                        </div>
+                    )
+                )}
             </div>
         </div>
     );
--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,52 +1,52 @@
 export const frontEndSkills = [
-    { name: 'HTML', icon: 'html.svg' },
-    { name: 'CSS', icon: 'css.svg' },
-    { name: 'JavaScript', icon: 'javascript.svg' },
-    { name: 'TypeScript', icon: 'typescript.svg' },
-    { name: 'React', icon: 'react.svg' },
-    { name: 'Next.js', icon: 'nextjs.svg' },
-    { name: 'TailwindCSS', icon: 'tailwindcss.svg' },
-    { name: 'MUI(旧Material UI)', icon: 'mui.svg' },
+    { name: 'HTML', icon: 'html.svg', link: 'https://developer.mozilla.org/ja/docs/Learn/Getting_started_with_the_web/HTML_basics' },
+    { name: 'CSS', icon: 'css.svg', link: 'https://developer.mozilla.org/ja/docs/Web/CSS' },
+    { name: 'JavaScript', icon: 'javascript.svg', link: 'https://developer.mozilla.org/ja/docs/Web/JavaScript' },
+    { name: 'TypeScript', icon: 'typescript.svg', link: 'https://www.typescriptlang.org/' },
+    { name: 'React', icon: 'react.svg', link: 'https://ja.reactjs.org/' },
+    { name: 'Next.js', icon: 'nextjs.svg', link: 'https://nextjs.org/' },
+    { name: 'TailwindCSS', icon: 'tailwindcss.svg', link: 'https://v2.tailwindcss.com/' },
+    { name: 'MUI(旧Material UI)', icon: 'mui.svg', link: 'https://mui.com/' },
 ];
 
 export const backEndSkills = [
-    { name: 'Node.js', icon: 'nodejs.svg' },
-    { name: 'Ruby', icon: 'ruby.svg' },
-    { name: 'Cache / IRIS', icon: 'iris.svg' },
-    { name: 'AWS Lambda', icon: 'lambda.svg' },
-    { name: 'AWS DynamoDB', icon: 'dynamodb.svg' },
+    { name: 'Node.js', icon: 'nodejs.svg', link: 'https://nodejs.org/ja/' },
+    { name: 'Ruby', icon: 'ruby.svg', link: 'https://www.ruby-lang.org/ja/' },
+    { name: 'Cache / IRIS', icon: 'iris.svg', link: 'https://www.intersystems.com/jp/data-platform/' },
+    { name: 'AWS Lambda', icon: 'lambda.svg', link: 'https://aws.amazon.com/jp/lambda/' },
+    { name: 'AWS DynamoDB', icon: 'dynamodb.svg', link: 'https://aws.amazon.com/jp/dynamodb/' },
 ];
 
 export const serverSkills = [
-    { name: 'Windows Server', icon: 'windows.svg' },
-    { name: 'Linux', icon: 'linux.svg' },
-    { name: 'AWS EC2', icon: 'ec2.svg' },
+    { name: 'Windows Server', icon: 'windows.svg', link: 'https://www.microsoft.com/ja-jp/windows-server' },
+    { name: 'Linux', icon: 'linux.svg', link: 'https://ja.wikipedia.org/wiki/Linux' },
+    { name: 'AWS EC2', icon: 'ec2.svg', link: 'https://aws.amazon.com/jp/ec2/' },
 ];
 
 export const devToolSkills = [
-    { name: 'Git', icon: 'git.svg' },
-    { name: 'SVN', icon: 'svn.svg' },
-    { name: 'Redmine', icon: 'redmine.svg' },
-    { name: 'Backlog', icon: 'backlog.svg' },
-    { name: 'Visual Studio Code', icon: 'vscode.svg' },
-    { name: 'Google Workspace', icon: 'googleworkspace.svg' },
-    { name: 'Notion', icon: 'notion.svg' },
+    { name: 'Git', icon: 'git.svg', link: 'https://git-scm.com/' },
+    { name: 'SVN', icon: 'svn.svg', link: 'https://tortoisesvn.net/support.html' },
+    { name: 'Redmine', icon: 'redmine.svg', link: 'https://redmine.jp/' },
+    { name: 'Backlog', icon: 'backlog.svg', link: 'https://backlog.com/ja/' },
+    { name: 'Visual Studio Code', icon: 'vscode.svg', link: 'https://azure.microsoft.com/ja-jp/products/visual-studio-code' },
+    { name: 'Google Workspace', icon: 'googleworkspace.svg', link: 'https://workspace.google.co.jp/intl/ja/' },
+    { name: 'Notion', icon: 'notion.svg', link: 'https://www.notion.so/ja-jp' },
 ];
 
 export const certifications = [
-    { name: 'ITパスポート', icon: 'ipa.svg' },
-    { name: '基本情報技術者', icon: 'ipa.svg' },
-    { name: 'AWS Cloud Practitioner', icon: 'awsclf.svg' },
-    { name: 'AWS Solutions Architect', icon: 'awssaa.svg' },
-    { name: 'AWS Developer', icon: 'awsdoa.svg' },
-    { name: 'AWS SysOps Administrator', icon: 'awssoa.svg' },
+    { name: 'ITパスポート', icon: 'ipa.svg', link: 'https://www3.jitec.ipa.go.jp/JitesCbt/' },
+    { name: '基本情報技術者', icon: 'ipa.svg', link: 'https://www.jitec.ipa.go.jp/1_11seido/fe.html' },
+    { name: 'AWS Cloud Practitioner', icon: 'awsclf.svg', link: 'https://aws.amazon.com/jp/certification/certified-cloud-practitioner/' },
+    { name: 'AWS Solutions Architect', icon: 'awssaa.svg', link: 'https://aws.amazon.com/jp/certification/certified-solutions-architect-associate/' },
+    { name: 'AWS Developer', icon: 'awsdoa.svg', link: 'https://aws.amazon.com/jp/certification/certified-developer-associate/' },
+    { name: 'AWS SysOps Administrator', icon: 'awssoa.svg', link: 'https://aws.amazon.com/jp/certification/certified-sysops-admin-associate/' },
 ];
 
 export const littleSkills = [
-    { name: 'Zabbix', icon: 'zabbix.svg' },
-    { name: 'Jenkins', icon: 'jenkins.svg' },
-    { name: 'Firebase', icon: 'firebase.svg' },
-    { name: 'Figma', icon: 'figma.svg' },
-    { name: 'Google Apps Script', icon: 'gas.svg' },
-    { name: 'MySQL', icon: 'mysql.svg' },
+    { name: 'Zabbix', icon: 'zabbix.svg', link: 'https://www.zabbix.com/jp' },
+    { name: 'Jenkins', icon: 'jenkins.svg', link: 'https://www.jenkins.io/' },
+    { name: 'Firebase', icon: 'firebase.svg', link: 'https://firebase.google.com/?hl=ja' },
+    { name: 'Figma', icon: 'figma.svg', link: 'https://www.figma.com/ja/' },
+    { name: 'Google Apps Script', icon: 'gas.svg', link: 'https://workspace.google.co.jp/intl/ja/products/apps-script/' },
+    { name: 'MySQL', icon: 'mysql.svg', link: 'https://www.mysql.com/jp/' },
 ];
