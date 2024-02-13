3ec918e415e2c69c580486b78055a16b94cc70b2
tukky-14 <bigoonage0104@gmail.com>
2024-02-13 21:51:11 +0900

    Worksを修正

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -33,7 +33,7 @@ const worksArray = [
         url: 'https://tukkytech.com/',
         title: 'Tukky Tech Blog',
         img: '/images/tukkytech.png',
-        description: '2つ目に作成した自身のブログサイトです。UIを1から自分で作成してみたかったため作成し直しました。',
+        description: '2つ目に作成した自身のブログサイトです。UIを1から作成し直しました。',
         tech: '使用技術：Next.js / TypeScript / microCMS / S3 / CloudFront',
     },
     {
@@ -49,10 +49,10 @@ const Works = () => {
     return (
         <div className="mb-40 pt-10" id="works">
             <h2 className="text-center text-2xl mb-8 m-auto font-bold border-b-4 w-1/2 border-blue-500">WORKS</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {worksArray.map((work, index) => (
                     <Link
-                        className="h-[400px] sm:h-[450px] border border-gray-200 rounded bg-white hover:scale-105 duration-300"
+                        className="h-[400px] lg:h-96 sm:h-[450px] border border-gray-200 rounded bg-white hover:scale-105 duration-300"
                         href={work.url}
                         target="_blank"
                         rel="noreferrer"
@@ -60,7 +60,7 @@ const Works = () => {
                     >
                         <p className="text-center mb-2 pt-2 pb-1 px-4 bg-blue-600 text-white font-bold">{work.title}</p>
                         <img className="border-b border-blue-200" src={work.img} alt={`${work.title}の画像`} />
-                        <div className="text-sm my-2 px-4 flex flex-col gap-1">
+                        <div className="text-sm my-2 px-4 flex flex-col gap-2">
                             <p>{work.description}</p>
                             <p>使用技術：{work.tech}</p>
                         </div>
